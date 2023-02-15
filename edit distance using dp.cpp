7661d4d080fdefee4dabedb8dd22830cdd16eace
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2023-02-15 12:37:18 +0600

    Add files via upload

---
+++ edit distance using dp.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int min(int x, int y, int z)
+{ return min(min(x, y), z); }
+
+int editDist(string str1, string str2, int m, int n)
+{
+
+    if (m == 0)
+        return n;
+
+
+    if (n == 0)
+        return m;
+
+
+    if (str1[m - 1] == str2[n - 1])
+        return editDist(str1, str2, m - 1, n - 1);
+
+    else
+
+    {return 1 + min(editDist(str1, str2, m, n - 1), // Insert
+                 editDist(str1, str2, m - 1, n), // Remove
+                 editDist(str1, str2, m - 1,
+                          n - 1) // Replace
+             );}
+}
+
+int main()
+{
+
+    string str1 ;
+    string str2 ;
+    cout<<"Enter 1st string :"<<'\n';
+    cin>>str1;
+    cout<<"Enter 2nd string :"<<'\n';
+    cin>>str2;
+
+    cout << editDist(str1, str2, str1.length(),
+                     str2.length());
+
+    return 0;
+}
